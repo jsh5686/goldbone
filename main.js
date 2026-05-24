3956d6f8be9138f1f0d7903fc612cb6d3a71ec85
jinseonghun72 <jinseonghun72@gmail.com>
2026-05-24 13:38:54 +0000

    Deploy lotto number generator

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Add JS here
+const numbersDiv = document.querySelector('.numbers');
+const generateBtn = document.querySelector('#generate');
+
+function generateNumbers() {
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    numbersDiv.innerHTML = '';
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    for (const number of sortedNumbers) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('number');
+        numberDiv.textContent = number;
+        numbersDiv.appendChild(numberDiv);
+    }
+}
+
+generateBtn.addEventListener('click', generateNumbers);
+
+generateNumbers();
